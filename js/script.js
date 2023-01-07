bf1317f3836256ae073c52ad6760b5a25b7af197
AntypeC <3c7975@gmail.com>
2023-01-08 02:21:08 +0800

    update

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.body.appendChild(renderer.domElement);
 camera.position.add(new THREE.Vector3(2, 2, 2))
 camera.rotation.y = Math.PI/4;
 
-const geometry = new THREE.BoxGeometry(1, 3, 1)
+const baseGeometry = new THREE.BoxGeometry(1, 3, 1)
 // mesh
 var material = new THREE.MeshPhongMaterial( {
     color: 0xff0000,
@@ -18,56 +18,75 @@ var material = new THREE.MeshPhongMaterial( {
     polygonOffsetFactor: 1, // positive value pushes polygon further away
     polygonOffsetUnits: 1
 } );
-var mesh = new THREE.Mesh( geometry, material );
+var mesh = new THREE.Mesh( baseGeometry, material );
 scene.add( mesh )
 
 // wireframe
-var edge = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
+var baseEdge = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
 var mat = new THREE.LineBasicMaterial( { color: 0xffffff} );
-var wireframe = new THREE.LineSegments( edge, mat );
+var wireframe = new THREE.LineSegments( baseEdge, mat );
 mesh.add( wireframe );
 
-var stackAdder = undefined
+var scanner = undefined
 var count = 0
+const scanRange = 1.5
 
-function AddStack() {
-    const geo = new THREE.BoxGeometry(1, 0.3, 1);
-    stackAdder = new THREE.Mesh(geo, material);
-    const e = new THREE.EdgesGeometry(stackAdder.geometry);
-    const w = new THREE.LineSegments(e, mat);
-    stackAdder.add(w);
-    stackAdder.position.y = count+1.65;
-    stackAdder.position.z = -1.5;
-
-    scene.add(stackAdder);
-    count += 0.3
+function ToVertices(geometry, zDelta) {
+    const positions = geometry.attributes.position;
+    const vertices = [];
+    for (let index = 0; index < positions.count; index++) {
+        vertices.push(
+            new THREE.Vector3(
+                positions.getX(index),
+                positions.getY(index),
+                positions.getZ(index)+zDelta
+            )
+        )
+    }
+    return vertices;
 }
 
-// var geometry1 = new THREE.BoxGeometry(1, 1, 1);
-// var geometry2 = new THREE.BoxGeometry(1, 1, 1);
+function addStack(intersection, displacement) {
+    const defaultGeometry = new THREE.BoxGeometry(1, 0.3, 1);
+    scanner = new THREE.Mesh(defaultGeometry, material);
+    const edge = new THREE.EdgesGeometry(scanner.geometry);
+    const wire = new THREE.LineSegments(edge, mat);
+    scanner.add(wire);
+    scanner.position.y = count+1.65;
+    scanner.position.z = -scanRange;
 
-// var csg1 = CSG.fromGeometry(geometry1);
-// var csg2 = CSG.fromGeometry(geometry2);
+    if (count!=0 && intersection != 0) {
+        const geometry = new THREE.BoxGeometry(1, 0.3, intersection)
+        const stack = new THREE.Mesh(geometry, material);
+        stack.add(new THREE.LineSegments(new THREE.EdgesGeometry(stack.geometry)));
+        stack.position.y = (count-0.3)+1.65;
+        stack.position.z = displacement;
+        scene.add(stack)
+    }
 
-// var result = csg1.intersect(csg2);
+    // const intersection = new THREE.Mesh(geometry, material);
+    // intersection.add(new THREE.LineSegments(new THREE.EdgesGeometry(intersection.geometry)));
+    // intersection.position.y = count+1.65;
 
-// var intersectionGeometry = result.toGeometry();
+    scene.add(scanner);
+    count += 0.3
+}
 
 scene.add(mesh);
 
 var goForward = true
 
 function animate() {
-    let speed = 0.02;
-    if (stackAdder != undefined) {
+    let speed = 0.008;
+    if (scanner != undefined) {
         if (goForward == true) {
-            stackAdder.position.z += speed;
-            if (stackAdder.position.z >= 1.5) {
+            scanner.position.z += speed;
+            if (scanner.position.z >= scanRange) {
                 goForward = false
             }
         } else {
-            stackAdder.position.z -= speed;
-            if (stackAdder.position.z <= -1.5) {
+            scanner.position.z -= speed;
+            if (scanner.position.z <= -scanRange) {
                 goForward = true
             }
         }
@@ -78,11 +97,30 @@ function animate() {
 
 document.addEventListener('keydown', (event) => {
     if (event.key == 'Spacebar' || event.key == ' ') {
-        AddStack()
-        if (document.getElementsByClassName('text') != null) {
+        camera.position.y += 0.05
+        let intersection = 0;
+        let displacement = 0;
+  
+        if (count == 0) {
             document.getElementById('score').remove();
             document.getElementById('how-to').remove();
+        } else {
+            scene.remove(scanner)
+            console.log(scanner.position.z)
+            let distanceFromCenter = Math.abs(scanner.position.z);
+            if (scanner.position.z <= 0) {
+                if (distanceFromCenter < 1) {
+                    intersection = 1- distanceFromCenter;
+                    displacement = -((0.5 - intersection) + 0.5*intersection);
+                }
+            } else {
+                if (distanceFromCenter < 1) {
+                    intersection = 1- distanceFromCenter;
+                    displacement = -((-0.5 + intersection) - 0.5*intersection);
+                }
+            }
         }
+        addStack(intersection, displacement)
     }
 })
 
