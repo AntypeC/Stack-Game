34f3c81538b9ebf10de1bd2815dfcfd566291e64
AntypeC <3c7975@gmail.com>
2023-01-08 19:26:08 +0800

    with lots of issues

--- js/script.js
+++ js/script.js
@@ -29,53 +29,64 @@ mesh.add( wireframe );
 
 var scanner = undefined
 var count = 0
-document.getElementById('score').innerHTML = 'Score: '+count/0.3
 const scanRange = 1.5
 
-function ToVertices(geometry, zDelta) {
-    const positions = geometry.attributes.position;
-    const vertices = [];
-    for (let index = 0; index < positions.count; index++) {
-        vertices.push(
-            new THREE.Vector3(
-                positions.getX(index),
-                positions.getY(index),
-                positions.getZ(index)+zDelta
-            )
-        )
-    }
-    return vertices;
-}
+/**
+ * Returns intersecting part of two rectangles
+ * @param  {object}  r1 4 coordinates in form of {x1, y1, x2, y2} object
+ * @param  {object}  r2 4 coordinates in form of {x1, y1, x2, y2} object
+ * @return {boolean}    False if there's no intersecting part
+ * @return {object}     4 coordinates in form of {x1, y1, x2, y2} object
+ */
+const getIntersectingRectangle = (r1, r2) => {  
+    [r1, r2] = [r1, r2].map(r => {
+    return {
+        x: [r.x1, r.x2].sort((a,b) => a - b),
+        y: [r.y1, r.y2].sort((a,b) => a - b)
+    };
+    });
 
-function addStack(intersection, displacement) {
+    const noIntersect = r2.x[0] > r1.x[1] || r2.x[1] < r1.x[0] ||
+                        r2.y[0] > r1.y[1] || r2.y[1] < r1.y[0];
+
+    return noIntersect ? false : {
+    x1: Math.max(r1.x[0], r2.x[0]), // _[0] is the lesser,
+    y1: Math.max(r1.y[0], r2.y[0]), // _[1] is the greater
+    x2: Math.min(r1.x[1], r2.x[1]),
+    y2: Math.min(r1.y[1], r2.y[1])
+    };
+};
+
+var rectangle1 = { x1: 0.5, y1: 0.5, x2: -0.5, y2: -0.5 }
+var rectangle2 = { x1: 0.5, y1: 0.5, x2: -0.5, y2: -0.5 }
+var intersect = null;
+
+function addStack(width, center) {
     const defaultGeometry = new THREE.BoxGeometry(1, 0.3, 1);
     scanner = new THREE.Mesh(defaultGeometry, material);
     const edge = new THREE.EdgesGeometry(scanner.geometry);
     const wire = new THREE.LineSegments(edge, mat);
     scanner.add(wire);
-    scanner.position.y = count+1.65;
+    scanner.position.y = (count*0.3)+1.65;
     scanner.position.z = -scanRange;
 
-    if (count!=0 && intersection != 0) {
-        const geometry = new THREE.BoxGeometry(1, 0.3, intersection)
+    if (count!=0) {
+        const geometry = new THREE.BoxGeometry(1, 0.3, width)
         const stack = new THREE.Mesh(geometry, material);
         stack.add(new THREE.LineSegments(new THREE.EdgesGeometry(stack.geometry)));
-        stack.position.y = (count-0.3)+1.65;
-        stack.position.z = displacement;
+        stack.position.y = (count*0.3 -0.3)+1.65;
+        stack.position.z = center;
+        rectangle2 = { x1: stack.position.z+0.5, y1: 0.5, x2: stack.position.z-0.5, y2: -0.5 };
         scene.add(stack)
     }
 
-    // const intersection = new THREE.Mesh(geometry, material);
-    // intersection.add(new THREE.LineSegments(new THREE.EdgesGeometry(intersection.geometry)));
-    // intersection.position.y = count+1.65;
-
     scene.add(scanner);
-    count += 0.3
 }
 
 scene.add(mesh);
 
 var goForward = true
+var moveCamera = 2
 
 function animate() {
     let speed = 0.008;
@@ -92,39 +103,54 @@ function animate() {
             }
         }
     }
+    if (camera.position.y < moveCamera) {
+        camera.position.y += 0.01
+    }
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
 
-var width = 0.5;
-var center = 0;
+animate();
+
+var elem = document.getElementById('info');
+var restart = document.getElementById('restart');
+
+restart.onclick = function reset() {
+    document.getElementById('red').style.display = 'none';
+    document.getElementById('death-msg').style.display = 'none';
+    document.getElementById('restart').style.display = 'none'
+    document.getElementById('message').style.display = 'block'
+}
 
 document.addEventListener('keydown', (event) => {
-    if (event.key == 'Spacebar' || event.key == ' ') {
-        camera.position.y += 0.08
-        let intersection = 0;
-        let displacement = 0;
-  
+    elem.innerHTML = 'Press space to stack!'
+    document.getElementById('score').innerHTML = 'Score: '+count
+    run: if (event.key == 'Spacebar' || event.key == ' ') {
         if (count == 0) {
-            document.getElementsByClassName('info')[0].remove();
+            moveCamera += 0.3
+            elem.style.display = 'none'
+            addStack()
         } else {
+            rectangle1 = { x1: scanner.position.z+0.5, y1: 0.5, x2: scanner.position.z-0.5, y2: -0.5 };
+            intersect = getIntersectingRectangle(rectangle1, rectangle2)
+            let width = Math.abs(intersect.x2-intersect.x1)
+            let position = (intersect.x1+intersect.x2)/2
             scene.remove(scanner)
-            console.log(scanner.position.z)
-            let distanceFromCenter = Math.abs(scanner.position.z);
-            if (scanner.position.z <= 0) {
-                if (distanceFromCenter < 1) {
-                    intersection = 1- distanceFromCenter;
-                    displacement = -((0.5 - intersection) + 0.5*intersection);
-                }
+            if (intersect == false) {
+                var redbg = document.getElementById('red-tint');
+                var deathmsg = document.getElementById('death-msg');
+                redbg.style.display = 'block';
+                deathmsg.style.display = 'block';
+                restart.style.display = 'block';
+                break run;
             } else {
-                if (distanceFromCenter < 1) {
-                    intersection = 1- distanceFromCenter;
-                    displacement = -((-0.5 + intersection) - 0.5*intersection);
-                }
+                moveCamera += 0.3
+                console.log('coords: ')
+                console.log(intersect)
+                addStack(width, position) 
             }
         }
-        addStack(intersection, displacement)
+        count += 1
     }
 })
 
-animate();
